95270ef169ae43c1628cf308bd1ce531b5e5b614
JackerNhatNguyen073 <nguyentranminhnhatkd@gmail.com>
2025-06-06 20:26:20 +0700

    feat: fix user, including api login, register, and change password

--- backend/controllers/user.controller.php
+++ backend/controllers/user.controller.php
@@ -1,30 +1,59 @@
 <?php
-require_once __DIR__.'/../services/user.services.php';
+require_once __DIR__ . '/../services/user.services.php';
+
 class UserController {
-    public static function register($data) {
-        $result = UserService::register(
-            trim($data['user_name']),
-            $data['password'],
-            $data['re_password']
-        );
-        echo json_encode($result);
+    private $service;
+
+    public function __construct($db) {
+        $this->service = new UserService($db);
     }
 
-    public static function login($data) {
-        $result = UserService::login(
-            trim($data['user_name']),
-            $data['password']
-        );
-        echo json_encode($result);
+    public function login(array $data) {
+        $user_name = $data['user_name'] ?? '';
+        $password = $data['password'] ?? '';
+
+        $user = $this->service->login($user_name, $password);
+        if ($user) {
+            return [
+                'message' => 'Đăng nhập thành công',
+                'user' => $user
+            ];
+        } else {
+            http_response_code(401);
+            return ['message' => 'Sai tài khoản hoặc mật khẩu'];
+        }
     }
 
-    public static function changePassword($data) {
-        $result = UserService::changePassword(
-            trim($data['user_name']),
-            $data['current_password'],
-            $data['new_password'],
-            $data['re_password']
-        );
-        echo json_encode($result);
+    public function register(array $data) {
+        $user_name = $data['user_name'] ?? '';
+    $password = $data['password'] ?? '';
+    $confirm = $data['re_password'] ?? '';
+
+    if ($password !== $confirm) {
+        http_response_code(400);
+        return ['message' => 'Mật khẩu xác nhận không khớp'];
+    }
+        $success = $this->service->register($user_name, $password,);
+        if ($success) {
+            return ['message' => 'Đăng ký thành công'];
+        } else {
+            http_response_code(409);
+            return ['message' => 'Tài khoản đã tồn tại'];
+        }
+    }
+
+    public function changePassword(array $data) {
+        $user_name = $data['user_name'] ?? '';
+        $old_pass = $data['old_password'] ?? '';
+        $new_pass = $data['new_password'] ?? '';
+
+        $success = $this->service->changePassword($user_name, $old_pass, $new_pass);
+        if ($success) {
+            return ['message' => 'Đổi mật khẩu thành công'];
+        } else {
+            http_response_code(403);
+            return ['message' => 'Mật khẩu cũ không đúng'];
+        }
     }
 }
+?>
